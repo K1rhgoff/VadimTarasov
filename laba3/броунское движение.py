58d9b1f797f35837d603b55e99bc9d5f6e84e089
K1rhgoff <tarasov.vadim@phystech.edu>
2021-10-14 18:36:40 +0300

    ok

---
+++ laba3/броунское движение.py
@@ -0,0 +1,5 @@
+from random import *
+import turtle
+for i in range(1000):
+    turtle.forward (20*random())
+    turtle.left (180*random())
--- броунское движение.py
+++
@@ -1,5 +0,0 @@
-from random import *
-import turtle
-for i in range(1000):
-    turtle.forward (20*random())
-    turtle.left (180*random())
